353765b4eea9453c170a65d25faf06554358888f
Callum Macalast <calmacalast@gmail.com>
2024-04-20 22:20:26 +0100

    install tailwind, update to app router, create boilerplate

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click me
+    </button>
+  )
+}
+
+export default Button
