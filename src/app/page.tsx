c15c67d0b495b97b84d10a7ebf48b79897437a57
Callum Macalast <calmacalast@gmail.com>
2024-04-21 11:12:19 +0100

    test connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Button } from '../components/atoms/Button'
+import clientPromise from '../../lib/mongodb'
 
-const page = () => {
+const page = async () => {
+  const connection = await clientPromise
+
+  console.log(connection)
+
   return (
     <div>
       <Button />
