86bda1b3c896f543504b74a53ff995a56e250d08
DylanMacalast <dylanmacalast@icloud.com>
2024-04-21 12:06:12 +0100

    adding this in

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import { bookRepo } from "../_helpers/server/book-repo";
+
+export default async function Page() {
+  const book = (await bookRepo.getAll())?.[0] ?? null;
+
+  return (
+    <>
+      <div>
+        <h1>{book?.title ?? "hi"}</h1>
+        <p>{book?.author ?? "hi"}</p>
+        {/* Add more fields as necessary */}
+      </div>
+    </>
+  );
+}
