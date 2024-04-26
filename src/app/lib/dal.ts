b27622742bae59a20400422a16b953a913571bfe
Callum Macalast <calmacalast@gmail.com>
2024-04-26 17:58:24 +0100

    create data access layer for session management

---
+++ src/app/lib/dal.ts
@@ -0,0 +1,16 @@
+import 'server-only';
+
+import { cookies } from 'next/headers';
+import { decrypt } from './session';
+import { cache } from 'react';
+
+export const verifySession = cache(async () => {
+  const cookie = cookies().get('session')?.value;
+  const session = await decrypt(cookie);
+
+  if (!session?.userId) {
+    return { isAuth: false };
+  }
+
+  return { isAuth: true, userId: session.userId };
+});
