eb467011ecfd12d5ad3cc506a01e2512db93b2ae
Callum Macalast <calmacalast@gmail.com>
2024-05-02 21:38:30 +0100

    design tweaks

--- src/components/atoms/Icons/index.ts
+++ src/components/atoms/Icons/index.ts
@@ -1,1 +1,2 @@
 export { default as UserIcon } from './UserIcon';
+export { default as UNLoginIcon } from './UNLoginIcon';
--- src/components/organisms/Forms/LoginForm/LoginForm.tsx
+++ src/components/organisms/Forms/LoginForm/LoginForm.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation';
 import React from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import { login } from '../../../../actions/auth';
+import Link from 'next/link';
 
 const LoginForm = () => {
   const inState = {
@@ -14,9 +15,9 @@ const LoginForm = () => {
   const router = useRouter();
 
   return (
-    <div className="max-w-2xl mx-auto">
-      <h1 className="text-2xl text-center my-2 font-semibold">Login</h1>
-      <form action={formAction}>
+    <div className="max-w-md w-full">
+      <h1 className="text-3xl text-center my-2 font-semibold">Login</h1>
+      <form action={formAction} className="mt-5">
         <div className="grid grid-cols-1 gap-3">
           <input
             placeholder="Username"
@@ -33,12 +34,21 @@ const LoginForm = () => {
             className="p-2 bg-white rounded focus:shadow focus:border-none  outline-none"
             required
           />
+          <div>
+            <Link href="#">Forgot password?</Link>
+          </div>
           <button
             type="submit"
-            className="bg-blue-400 rounded shadow text-white p-2"
+            className="bg-blue-400 rounded shadow text-white p-2 "
           >
             {pending ? 'Loading...' : 'Login'}
           </button>
+          <div>
+            Don't have an account?{' '}
+            <Link href="/register" className="text-blue-500 font-bold">
+              Register
+            </Link>
+          </div>
         </div>
         <span className="text-red-400 text-xs">{state?.message}</span>
       </form>
