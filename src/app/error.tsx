108a40104695c3a8c26dee21b046a8e5052ee6d0
Callum Macalast <calmacalast@gmail.com>
2024-05-02 21:18:56 +0100

    remove comment

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -13,10 +13,7 @@ export default function Error({
       <h2 className="text-center">Something went wrong!</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          // Attempt to recover by trying to re-render the invoices route
-          () => reset()
-        }
+        onClick={() => reset()}
       >
         Try again
       </button>
